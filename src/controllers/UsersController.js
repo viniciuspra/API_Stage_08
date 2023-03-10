20a23e148fc02b6cf1018d2e2d5d810bcfa20429
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-10 15:49:38 -0300

    Update UsersController.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
   create(req, res) {
     const { name, email, password } = req.body
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório!")
+    }
 
     res.status(201).json({ name, email, password })
   }
