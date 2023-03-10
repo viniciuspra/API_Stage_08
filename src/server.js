08383a068607afd19e964e0932e1e7dd59382911
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-10 15:49:32 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+require("express-async-errors")
+const AppError = require("./utils/AppError")
 const express = require('express');
 const routes = require("./routes")
 
 const app = express();
 app.use(express.json())
 app.use(routes)
+app.use(error, req, res, next)
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
