a31bbf9839b2b954ca2f667fc30e12142611b657
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-13 17:53:38 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
-require("express-async-errors")
-const AppError = require("./utils/AppError")
+require("express-async-errors");
+const AppError = require("./utils/AppError");
 const express = require('express');
-const routes = require("./routes")
+const routes = require("./routes");
 
 const app = express();
-app.use(express.json())
-app.use(routes)
-app.use(error, req, res, next)
+app.use(express.json());
+app.use(routes);
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ 
+      status: "error",
+      message: error.message
+     })
+  };
+
+  console.log(error);
+
+  return res.status(500).json({ 
+    status: "error",
+    message: "Internal server error",
+   });
+
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
