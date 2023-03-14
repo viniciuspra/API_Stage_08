26ff0f0d88c7e787b8dbcaf862cba6f8782b5aba
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-13 22:00:43 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
-require("express-async-errors");
-const AppError = require("./utils/AppError");
-const express = require('express');
-const routes = require("./routes");
+require("express-async-errors")
+const migrationsRun = require("./database/sqlite/migrations")
+const AppError = require("./utils/AppError")
+const express = require("express")
+const routes = require("./routes")
 
-const app = express();
-app.use(express.json());
-app.use(routes);
+migrationsRun()
+
+const app = express()
+app.use(express.json())
+app.use(routes)
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
-    return res.status(error.statusCode).json({ 
+    return res.status(error.statusCode).json({
       status: "error",
-      message: error.message
-     })
-  };
+      message: error.message,
+    })
+  }
 
-  console.log(error);
+  console.log(error)
 
-  return res.status(500).json({ 
+  return res.status(500).json({
     status: "error",
     message: "Internal server error",
-   });
+  })
+})
 
-});
-
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
