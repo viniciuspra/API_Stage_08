8c77bb359f2693c0d7b1e2b12503af4ad2841bd3
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-10 17:56:31 -0300

    feat: Use User Repository

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,16 @@
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
+
+const UserRepository = require("../repositories/UserRepository")
 const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
-    const database = await sqliteConnection()
-    const checkUserExists = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    )
+    const userRepository = new UserRepository()
+
+    const checkUserExists = await userRepository.findByEmail(email)
 
     if (checkUserExists) {
       throw new AppError("Este e-mail já está em uso.")
@@ -18,10 +18,7 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8)
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]
-    )
+    await userRepository.create({ name, email, password: hashedPassword })
 
     return response.status(201).json()
   }
@@ -39,7 +36,7 @@ class UsersController {
 
     const userWithUpdatedEmail = await database.get(
       "SELECT * FROM users WHERE email = (?)", [email]
-      )
+    )
 
     if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Este e-mail já está em uso.")
